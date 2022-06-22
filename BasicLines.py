389acff7550385339599cf7206a4dc97923f8999
Mel1Am <mel1am@hotmail.com>
2022-06-22 15:39:27 -0500

    Function creation

--- BasicLines.py
+++
@@ -1,9 +0,0 @@
-num1= 10
-num2= 5
-
-print(num1 + num2)
-
-lista = (1,2,3,4,5)
-print(len(lista))
-
-
---
+++ Function_creation.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+a = int(input("Give me a value: "))
+b = int(input("Give me a second value: "))
+
+def suma(a, b):
+    values=a+b
+    print(values)
+
+suma(a,b)
